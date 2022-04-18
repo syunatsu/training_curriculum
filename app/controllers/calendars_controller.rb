fda9f4ea975238addf21b54504b18456023719b0
syunatsu <myf.tyys@gmail.com>
2022-04-18 16:14:14 +0900

    Issue6の解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,12 +35,13 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday
+      wday_num = @todays_date.wday
       if wday_num >= 7
         wday_num = wday_num - 7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num-7+x]}
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[(@todays_date+x).wday]}
 
       @week_days.push(days)
     end
